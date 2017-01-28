f226634d30548250681d726941b33548c2e60f62
Joel Franke <joelfranke@gmail.com>
2017-01-28 19:04:20 +0100

    Add files via upload

--- options.js
+++ options.js
@@ -1,11 +1,17 @@
 // Saves options to chrome.storage.sync.
 function save_options() {
+ 
  //var attributes = document.getElementById('attributes').value;
   var portfolio = document.getElementById('portfolio').value;
+  
+  //scrub the portfolio variable of all numbers and punctuation, except comma (,)
+  portfolio = portfolio.replace(/[^A-Za-z,]/g,'');
+  console.log(portfolio)
   chrome.storage.sync.set({
 	portfolio: portfolio
   }, function() {
-    // Update status to let user know options were saved.
+    
+	// Update status to let user know options were saved.
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(function() {
@@ -13,14 +19,15 @@ function save_options() {
     }, 1000);
   });
 }
+
 // stored in chrome.storage.
-function restore_options() {
+function restored_options() {
   chrome.storage.sync.get({
 	portfolio: null
   }, function(items) {
 	document.getElementById('portfolio').value = items.portfolio;
   });
 }
-document.addEventListener('DOMContentLoaded', restore_options);
+document.addEventListener('DOMContentLoaded', restored_options);
 document.getElementById('save').addEventListener('click',
     save_options);
